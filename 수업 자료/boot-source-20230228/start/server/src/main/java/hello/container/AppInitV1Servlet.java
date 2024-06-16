210395d7c211c594bd0a1c602e3c368abfa7b738
eodevelop <eodevelop@naver.com>
2024-06-16 18:09:54 +0900

    임시 저장

--- 수업 자료/boot-source-20230228/start/server/src/main/java/hello/container/AppInitV1Servlet.java
+++ 수업 자료/boot-source-20230228/start/server/src/main/java/hello/container/AppInitV1Servlet.java
@@ -12,6 +12,7 @@ public class AppInitV1Servlet implements AppInit {
         System.out.println("AppInitV1Servlet.onStartup");
 
         // 순수 서블릿 코드 등록
+        // 단축키 Ctrl + Alt + V : 변수 추출
         ServletRegistration.Dynamic helloServlet = servletContext.addServlet("helloServlet", new HelloServlet());
         helloServlet.addMapping("/hello-servlet");
     }
--- 수업 자료/boot-source-20230228/start/server/src/main/java/hello/container/MyContainerInitV2.java
+++ 수업 자료/boot-source-20230228/start/server/src/main/java/hello/container/MyContainerInitV2.java
@@ -3,15 +3,27 @@ package hello.container;
 import jakarta.servlet.ServletContainerInitializer;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.HandlesTypes;
 
 import java.util.Set;
 
-public class MyContainerInitV1 implements ServletContainerInitializer {
+@HandlesTypes(AppInit.class)
+public class MyContainerInitV2 implements ServletContainerInitializer {
 
     @Override
     public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
-        System.out.println("MyContainerInitV1.onStartup");
-        System.out.println("MyContainerInitV1 c = " + c);
-        System.out.println("MyContainerInitV1 ctx = " + ctx);
+        System.out.println("MyContainerInitV2.onStartup");
+        System.out.println("MyContainerInitV2 c = " + c);
+        System.out.println("MyContainerInitV2 ctx = " + ctx);
+
+        for (Class<?> aClass : c) {
+            try {
+                // new AppInitV1Servlet() 과 같은 코드
+                AppInit appInit = (AppInit) aClass.getDeclaredConstructor().newInstance();
+                appInit.onStartup(ctx);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
