7adefc59927a098224611d0e4b48346ca9881cb2
eodevelop <eodevelop@naver.com>
2024-07-28 18:37:26 +0900

    테스트겸 직접 만든 셋 커밋

--- 실전 자바 - 중급 2편/java-mid2/src/collection/set/MyHashSetV0_Copy.java
+++ 실전 자바 - 중급 2편/java-mid2/src/collection/set/MyHashSetV0_Copy.java
@@ -4,5 +4,38 @@ import java.util.Arrays;
 
 public class MyHashSetV0_Copy {
 
-    private int[] elementData
+    private int[] elementData = new int[10];
+    private int size = 0;
+
+    // O(n)
+    public boolean add(int value) {
+        if (contains(value)) {
+            return false;
+        }
+        elementData[size] = value;
+        size++;
+        return true;
+    }
+
+    // O(n)
+    public boolean contains(int value) {
+        for (int data : elementData) {
+            if(data == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "MyHashSetV0_Copy{" +
+                "elementData=" + Arrays.toString(Arrays.copyOf(elementData, size)) +
+                ", size=" + size +
+                '}';
+    }
 }
