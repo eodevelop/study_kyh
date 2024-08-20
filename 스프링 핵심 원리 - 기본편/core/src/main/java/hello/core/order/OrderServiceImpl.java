1c28664097def89a752f05a4143e0e7267bf1f6e
eodevelop <eodevelop@naver.com>
2024-08-20 13:32:40 +0900

    새로운 할인 정책 개발

--- 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,12 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // 이 부분을 안고치고 변경 가능하게 하려면?
+    // 인터페이스의 구현 부분을 특정 클래스에 의존하는 코드가 여기에 있으면 안된다.
+    // DIP를 위반한것. 결국 인터페이스에만 의존해야한다.
+    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // 이 문제를 해결하기 위해선 누군가 대신 discountPolicy를 생성해서 주입해줘야 한다.
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
