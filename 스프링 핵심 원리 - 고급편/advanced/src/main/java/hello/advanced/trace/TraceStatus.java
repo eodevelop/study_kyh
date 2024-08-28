a9082bb935dc434fcebffd8a29b3d416f7c91aa2
eodevelop <eodevelop@naver.com>
2024-08-28 22:30:47 +0900

    프로토타입 개발

---
+++ 스프링 핵심 원리 - 고급편/advanced/src/main/java/hello/advanced/trace/TraceStatus.java
@@ -0,0 +1,26 @@
+package hello.advanced.trace;
+
+// 로그 시작시 상태정보를 가지고 있는 클래스
+public class TraceStatus {
+    private TraceId traceId;
+    private Long startTimeMs;
+    private String message;
+
+    public TraceStatus(TraceId traceId, Long startTimeMs, String message) {
+        this.traceId = traceId;
+        this.startTimeMs = startTimeMs;
+        this.message = message;
+    }
+
+    public TraceId getTraceId() {
+        return traceId;
+    }
+
+    public Long getStartTimeMs() {
+        return startTimeMs;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
