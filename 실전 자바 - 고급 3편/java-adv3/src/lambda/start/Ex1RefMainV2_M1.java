aa7eb716ab2f52058efc74941fca617f119b9156
eodevelop <eodevelop@naver.com>
2025-07-13 23:29:46 +0900

    - 자바 고급 3. 섹션2 강의 3 완료

---
+++ 실전 자바 - 고급 3편/java-adv3/src/lambda/start/Ex1RefMainV2_M1.java
@@ -0,0 +1,34 @@
+package lambda.start;
+
+import lambda.Procedure_M1;
+
+import java.util.Random;
+
+public class Ex1RefMainV2_M1 {
+
+    public static void hello(Procedure_M1 procedure) {
+        long startNs = System.nanoTime();
+        procedure.run();
+        long endNs = System.nanoTime();
+        System.out.println("실행 시간: " + (endNs - startNs) + "ns");
+    }
+
+    public static void main(String[] args) {
+        Procedure_M1 dice = new Procedure_M1() {
+            public void run() {
+                int randomValue = new Random().nextInt(6) + 1;
+                System.out.println("주사위 = " + randomValue);
+            }
+        };
+        hello(dice);
+        Procedure_M1 sum = new Procedure_M1() {
+            public void run() {
+                for (int i = 1; i <= 3; i++) {
+                    System.out.println("i = " + i);
+                }
+            }
+        };
+        hello(sum);
+    }
+
+}
---
+++ 실전 자바 - 고급 3편/java-adv3/src/lambda/start/Ex1RefMainV4_M1.java
@@ -0,0 +1,28 @@
+package lambda.start;
+
+import lambda.Procedure_M1;
+
+import java.util.Random;
+
+public class Ex1RefMainV4_M1 {
+
+    public static void hello(Procedure_M1 procedure) {
+        long startNs = System.nanoTime();
+        procedure.run();
+        long endNs = System.nanoTime();
+        System.out.println("실행 시간: " + (endNs - startNs) + "ns");
+    }
+
+    public static void main(String[] args) {
+        hello(() -> {
+            int randomValue = new Random().nextInt(6) + 1;
+            System.out.println("주사위 = " + randomValue);
+        });
+        hello(() -> {
+            for (int i = 1; i <= 3; i++) {
+                System.out.println("i = " + i);
+            }
+        });
+    }
+
+}
